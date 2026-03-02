aec904ddc89a816ca355aa8a4800068c771afe11
Matej Bačo <matejbaco2000@gmail.com>
2026-03-02 22:55:24 +0100

    Disable buggy transitions

--- src/lib/viewTransition.ts
+++ src/lib/viewTransition.ts
@@ -1,14 +1,3 @@
-import { flushSync } from "react-dom";
-
 export function withViewTransition(fn: () => void | Promise<void>) {
-	if (document.startViewTransition) {
-		document.startViewTransition(() => {
-			document.documentElement.classList.add("vt-active");
-			flushSync(() => {
-				fn();
-			});
-		});
-	} else {
-		fn();
-	}
+	fn();
 }
