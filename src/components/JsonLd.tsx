7a9971b805e101a9cac8c3e41549da3f64edb661
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2026-05-07 07:58:03 +0530

    Fix npm check issues

--- src/components/JsonLd.tsx
+++ src/components/JsonLd.tsx
@@ -2,10 +2,5 @@
  * Renders JSON-LD structured data for SEO. Place in body; search engines parse it.
  */
 export default function JsonLd({ data }: { data: object }) {
-	return (
-		<script
-			type="application/ld+json"
-			dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
-		/>
-	);
+	return <script type="application/ld+json">{JSON.stringify(data)}</script>;
 }
